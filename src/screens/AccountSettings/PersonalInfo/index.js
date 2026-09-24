4f6b028bf569d313e559c0bb17257f81c4c0023e
4dithyan <mailforadithyan@gmail.com>
2026-09-24 13:32:09 +0530

    I_SEP_093 fixed

--- src/screens/AccountSettings/PersonalInfo/index.js
+++ src/screens/AccountSettings/PersonalInfo/index.js
@@ -576,10 +576,6 @@ const PersonalInfo = () => {
           >
             {updating ? "Saving..." : "Save changes"}
           </button>
-          <button className={styles.clear} type="button" onClick={fetchProfile}>
-            <Icon name="close" size="16" />
-            Reset changes
-          </button>
         </div>
         {success && (
           <div className={styles.successMessage}>
